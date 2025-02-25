6bea170147be035ac93db4034c32b1306a9ed95f
Dave Smith <davewsmith@gmail.com>
2025-02-24 20:22:23 -0800

    start plumbing in db/migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        from sqlalchemy import event  # noq
+        from sqlalchemy.engine import Engine  # noqa
+
+        @event.listens_for(Engine, 'connect')
+        def _sqlite_pragma(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGME foreign_keys=ON;")
+            cursor.close()
+
+    migrate.init_app(app, db, render_as_batch=True)
 
     from app.main import bp as main_bp  # noqa
     app.register_blueprint(main_bp)
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+
 bp = Blueprint('main', __name__, template_folder='templates')
 
+from app.main import models  # noqa
 from app.main import routes  # noqa
