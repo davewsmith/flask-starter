6d69c10982158a623787556a3718f44521280865
Dave Smith <davewsmith@gmail.com>
2025-02-24 21:10:54 -0800

    add a simple test of User

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,19 +1,22 @@
 import pytest
 
-from app import create_app
+from app import create_app, db
 from config import Config
 
 
 class TestConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
 @pytest.fixture()
 def flask_app():
     app = create_app(TestConfig)
-    # setUp TBD
-    yield app
-    # tearDown TBD
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
 
 
 @pytest.fixture()
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,4 +1,22 @@
+import sqlalchemy as sa
+
+from app import db
+from app.main.models import User
+
+
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'<!DOCTYPE html>' in response.data
+
+
+def test_user(client):
+    user = User(username='test', email='test@example.com')
+    db.session.add(user)
+    db.session.commit()
+
+    query = sa.select(User)
+    users = db.session.scalars(query).all()
+    assert len(users) == 1
+    assert users[0].username == 'test'
+    assert users[0].email == 'test@example.com'
